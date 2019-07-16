8c2ea8256e47705f6604f7e2792bf832f4c3a8df
Kannandevops <48170002+Kannandevops@users.noreply.github.com>
2019-07-16 18:56:11 +0530

    Add files via upload

---
+++ Master.ps1
@@ -0,0 +1,32 @@
+﻿$BASE_DIR='C:\'
+
+$sourceFile = '\\jda-hmm\c$\AutoScripts.zip'
+
+$targetFolder = $BASE_DIR + 'BuildConfiguration'
+
+[System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
+
+[System.IO.Compression.ZipFile]::ExtractToDirectory($sourceFile, $targetFolder)
+
+
+$ddMMyyyy=(Get-Date).ToString('dd-MM-yyyy');
+
+$MASTER_LOG_FILE=$BASE_DIR + "BuildConfiguration\output" + "Master_Log-"+$ddMMyyyy +".log"
+
+write-output "$(get-date) : initiating the configuration updates " | out-file $MASTER_LOG_FILE -Append -Force;  
+
+cd $BASE_DIR"BuildConfiguration"
+
+.\configurationupdates.ps1
+
+write-output "$(get-date) : Starting the IIS reset  " | out-file $MASTER_LOG_FILE -Append -Force;  
+
+cd $BASE_DIR"BuildConfiguration"
+
+.\IISRESTART.ps1
+
+write-output "$(get-date) : Starting the tomcat reset  " | out-file $MASTER_LOG_FILE -Append -Force;  
+
+cd $BASE_DIR"BuildConfiguration"
+
+.\TomcatRestart.ps1
